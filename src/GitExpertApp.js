d037022919ac97402ec5c5e2c6ce9c2c7e5dcaa5
Cesar2k94 <juliomederosarias@gmail.com>
2020-12-30 15:50:47 -0300

    agregando teeam

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const GitExpertApp = () => {
+
+    //const categorias = ['Chelsea', 'Arsenal', 'Leeds'];
+   
+    const [categorias, setCategorias] = useState(['Chelsea', 'Arsenal', 'Leeds']);
+
+     const handleAdd = () =>{
+        setCategorias(categ =>[ ...categ, "Fulham"]); 
+     }
+
     return (
         <>
           <h2>GitExpertApp</h2>
           <hr />  
+          <button onClick= {handleAdd} >Agregar</button>
+          <ol>
+              {
+                  categorias.map( category =>{
+                      return<li key={category} >{category}</li>
+                  })
+              }
+          </ol>
         </>
     )
 }
-
    
