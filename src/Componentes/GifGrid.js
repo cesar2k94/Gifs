01b5c95a46194c9d4aa86f059019980a45191661
Cesar2k94 <juliomederosarias@gmail.com>
2021-01-07 12:35:39 -0300

    Título de img con lista ordenada

--- src/Componentes/GifGrid.js
+++ src/Componentes/GifGrid.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 export const GifGrid = ( {categoria} ) => {
+
+const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        getGifs();
+    }, [])
     
     const getGifs = async()=>{
 
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=lzvjyAmtxakRcsf2YOwHA5IJL27NU29D'
         const resp = await fetch (url);  
         const {data} = await resp.json();
+        //const {title}= await resp.json();
         
         const gifs = data.map( img =>{
             return {
@@ -16,7 +24,8 @@ export const GifGrid = ( {categoria} ) => {
             }
         })  
     
-        console.log(gifs);
+        //console.log(gifs);
+        setImages(gifs);
 
     }
 
@@ -25,6 +34,19 @@ export const GifGrid = ( {categoria} ) => {
     return (
         <div>
             <h3>{categoria}</h3>
+            <ol>
+                {
+                    // images.map(img=>(
+                    //     <li key={img.id}>{img.title}</li>
+                       
+                    // ))
+
+                    //Con desestructuracion 
+                    images.map(({id,title})=>(
+                        <li key={id}>{title}</li>   
+                    ))
+                }
+            </ol>
         </div>
     )
 }
---
+++ src/Componentes/GifGridItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const GifGridItem = (img) => {
+
+    console.log(img);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
